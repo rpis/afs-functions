13ef90aeea1ab2d778f6aa13572462eafa34710f
rpis <rpis9999@gmail.com>
2023-02-20 13:44:05 +0000

    initial version

--- get-all/index.js
+++ get-all/index.js
@@ -1,6 +1,9 @@
 const { TableClient } = require("@azure/data-tables");
+const { DefaultAzureCredential } = require("@azure/identity");
 
-const tableClient = new TableClient(process.env.TABLE_CONNECTION_STRING, "users");
+const tableClient = new TableClient(process.env.TABLE_CONNECTION_STRING, "users",
+    new DefaultAzureCredential());
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -12,5 +15,5 @@ module.exports = async function (context, req) {
     context.res = {
         status: 200, /* Defaults to 200 */
         body: response
-    }; 
+    };
 }
--- get-one/index.js
+++ get-one/index.js
@@ -1,6 +1,8 @@
 const { TableClient } = require("@azure/data-tables");
+const { DefaultAzureCredential } = require("@azure/identity");
 
-const tableClient = new TableClient(process.env.TABLE_CONNECTION_STRING, "users");
+const tableClient = new TableClient(process.env.TABLE_CONNECTION_STRING, "users",
+    new DefaultAzureCredential());
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
